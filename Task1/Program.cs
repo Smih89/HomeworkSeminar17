3714782efe4217b0753e706d68a4a8b0b1d22203
SergeyMikheev <s.a.mikheev@yandex.ru>
2023-04-16 21:00:56 +0300

    apdate Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World1");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+string[] array = { "Hello", "2", "world", ":-" };
+// string[] array = {"1234", "1567", "-2", "computer science"};
+// string[] array = {"Russia", "Denmark", "Kazan"};
+
+string[] result = new string[array.Length];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result[count] = array[i];
+        count++;
+    }
+}
+Console.WriteLine(string.Join(" ",result));
+Console.ReadKey();
